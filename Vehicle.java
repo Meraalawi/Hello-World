f9330a8e1892556e94aad481664ad74ee41670d4
Mira mamoun alawi <119082568+Meraalawi@users.noreply.github.com>
2024-02-06 22:56:16 +0200

    Add files via upload
    This is Vehicle class not sure about constructor

---
+++ Vehicle.java
@@ -0,0 +1,29 @@
+class Vehicle extends Automobile {
+    private double Width;
+    private double Length;
+    private Color Color;
+
+    public double getWidth() {
+        return Width;
+    }
+
+    public void setWidth(double Width) {
+        this.Width = Width;
+    }
+
+    public double getLength() {
+        return Length;
+    }
+
+    public void setLength(double Length) {
+        this.Length = Length;
+    }
+
+    public Color getColor() {
+        return Color;
+    }
+
+    public void setColor(Color Color) {
+        this.Color = Color;
+    }
+}
